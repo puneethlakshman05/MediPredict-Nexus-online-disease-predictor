1cccfe9f18af39c75c5c421c86748630ba183232
puneethlakshman05 <puneethlaksh05gmail.com>
2025-08-11 16:30:19 +0530

    final deployment backend,frontend

--- src/config.js
+++ src/config.js
@@ -3,6 +3,5 @@ const isProduction = import.meta.env.PROD;  // Vite sets this automatically
 const API_BASE_URL = isProduction 
   ? import.meta.env.VITE_API_BASE_URL_PROD 
   : import.meta.env.VITE_API_BASE_URL_DEV;
-
-export default API_BASE_URL;
 console.log("API_BASE_URL in runtime:", API_BASE_URL);
+export default API_BASE_URL;
